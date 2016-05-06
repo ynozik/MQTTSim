aa2b571a5c14c0fd76c2fa3a77ce49334592d5f4
jon <jonnozik@gmail.com>
2016-05-06 14:41:07 -0700

    Create file structure using Maven; add mongodb java dependency.

---
+++ src/main/java/nozik/app/App.java
@@ -0,0 +1,29 @@
+package nozik.app;
+import com.mongodb.BasicDBObject;
+import com.mongodb.BulkWriteOperation;
+import com.mongodb.BulkWriteResult;
+import com.mongodb.Cursor;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.ParallelScanOptions;
+import com.mongodb.ServerAddress;
+
+import java.util.List;
+import java.util.Set;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Hello World!");
+    }
+}
