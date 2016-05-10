6ceac384d535784c26bfdf37eb4475605ccf9f03
jon <jonnozik@gmail.com>
2016-05-10 10:56:47 -0700

    Fix package errors, create compile-working mogno test code incorporated from the quickstart

--- src/main/java/nozik/app/App.java
+++ src/main/java/nozik/app/App.java
@@ -1,13 +1,32 @@
+package nozik;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.net.UnknownHostException;
+
 import com.mongodb.*;
 
 public class App {
   public static void main(String[] args) {
-    try {
-      MongoClient mongo = new MongoClient("localhost", 27017);
-    }
-    catch (Exception e) {
+    System.out.println("Beginning connection to local database");
+    MongoClient mongo = new MongoClient("localhost", 27017);
 
-    }
+    System.out.println("Get the database");
+    MongoDatabase database = mongo.getDatabase("test");
 
+    System.out.println("Get the collection");
+    MongoCollection<Document> collection = database.getCollection("collec");
+
+    System.out.println("Collection has " + collection.count() + " documents");
+
+    System.out.println("Close the client");
+    mongo.close();
+    System.out.println("Done");
   }
 }
